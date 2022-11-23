99ec1fcb7f768b94b0ff42d162855d24f172fcda
SabiasQue.Space <112111610+SabiasQueSpace@users.noreply.github.com>
2022-11-23 12:29:53 +0100

    pool_hashrate_graph_init

--- web/yaamp/modules/site/mining_lte.php
+++ web/yaamp/modules/site/mining_lte.php
@@ -178,56 +178,134 @@ function graph_init_price(data)
 
 ///////////////////////////////////////////////////////////////////////
 
-function pool_hashrate_ready(data)
-{
-	pool_hashrate_graph_init(data);
-}
-
-function pool_hashrate_refresh()
-{
-	var url = "/site/graph_hashrate_results";
-	$.get(url, '', pool_hashrate_ready);
-}
-
 function pool_hashrate_graph_init(data)
 {
+	
 	$('#pool_hashrate_results').empty();
+    data = JSON.parse(data);
+    var data1 = data[0];
+    var data2 = data[1];
+    var options = {
+					series : [{
+						name : 'Hashrate',
+						data : data1,
+					},{
+						name : 'Average',
+						data : data2,
+					}],
+					chart : {
+						type : "area",
+						height : 240,
+						foreColor : "#FFF",
+						fontFamily : 'Tahoma, Arial',
+						stacked : false,
+						dropShadow : {
+							enabled : true,
+							enabledSeries : [0],
+							top : -2,
+							left : 2,
+							blur : 5,
+							opacity : 0.03
+						},
+						toolbar : {
+							show : true,
+						}
+					},
+					colors : ['#008BB2','#830000'],
+					stroke : {
+						curve : "smooth",
+						width : 2
+					},
+					dataLabels : {
+						enabled : false
+					},
 
-	var t = $.parseJSON(data);
-	var plot1 = $.jqplot('pool_hashrate_results', t,
-	{
-		title: '<b>Pool Hashrate ({$algo_unit}/s)</b>',
-		axes: {
-			xaxis: {
-				tickInterval: 7200,
-				renderer: $.jqplot.DateAxisRenderer,
-				tickOptions: {formatString: '<font size=1>%#Hh</font>'}
-			},
-			yaxis: {
-				min: 0,
-				tickOptions: {formatString: '<font size=1>%#.3f &nbsp;</font>'}
-			}
-		},
-
-		seriesDefaults:
-		{
-			markerOptions: { style: 'none' }
-		},
-
-		grid:
-		{
-			borderWidth: 1,
-			shadowWidth: 0,
-			shadowDepth: 0,
-			background: '#41464b'
-		},
-
-		highlighter:
-		{
-			show: true
-		},
-
-	});
+					markers : {
+						size : 0,
+						strokeColor : "#000",
+						strokeWidth : 2,
+						strokeOpacity : 1,
+						fillOpacity : 1,
+						hover : {
+							size : 6
+						}
+					},
+					xaxis : {
+						type : "datetime",
+						axisBorder : {
+							show : true
+						},
+						axisTicks : {
+							show : true
+						}
+					},
+yaxis : {
+                        decimalsInFloat : 3,
+						labels : {
+							minWidth: 50,
+							"formatter": function (val) {
+								if(val> 1000000000){
+									val = val / 1000000000;
+									return val.toFixed(2) +" PH/s"
+								}
+								if(val> 1000000){
+									val = val / 0000000;
+									return val.toFixed(2) +" TH/s"
+								}
+								if(val> 1000){
+									val = val / 1000;
+									return val.toFixed(2) +" GH/s"
+								}
+								if(val> 1){
+									val = val / 1;
+									return val.toFixed(2) +" MH/s"
+								}
+								if(val> 0.001){
+									val = val / 0.001;
+									return val.toFixed(2) +" KH/s"
+								}else {
+									val = val / 0.000001;
+									return val.toFixed(2) +" H/s"
+								}
+            						},
+							offsetX : -10,
+							offsetY : 0
+						}
+					},
+					grid : {
+            borderColor: "#555555",
+						show : true,
+						padding : {
+							left : 0,
+							right : 0
+						}
+					},
+					tooltip : {
+						x : {
+							format : "dd MMM yyyy"
+						},
+                        theme : "dark",
+					},
+					legend : {
+						position : 'top',
+						horizontalAlign : 'left'
+					},
+					fill : {
+						type : 'gradient',
+						gradient : {
+							shadeIntensity : 1,
+							opacityFrom : 0.5,
+							opacityTo : 0,
+							stops : [0, 100, 100]
+						}
+					},
+				};
+				    
+                var chart = new ApexCharts(document.querySelector("#pool_hashrate_results"), options);
+				if (!chart.chartRendered || typeof chart.chartRendered != 'undefined') {
+					console.log(chart.chartRendered);
+            		chart.render().then(() => chart.chartRendered = true);
+				}			
 }
 
 </script>
